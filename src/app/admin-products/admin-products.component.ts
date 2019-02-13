60aace817fa5ef8e09321b0dac5f1d3a29504b7f
kamel-b-tahar.khenis <kamel-b-tahar.khenis@akka.eu>
2019-02-13 11:06:02 +0100

    add Crud on products

--- src/app/admin-products/admin-products.component.ts
+++ src/app/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CatalogueService } from '../catalogue.service';
 
 @Component({
   selector: 'app-admin-products',
@@ -6,10 +7,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-products.component.css']
 })
 export class AdminProductsComponent implements OnInit {
+  produits;
+  mode = 'list';
+  currentProduit;
 
-  constructor() { }
+  constructor(private catalogueService: CatalogueService) { }
 
   ngOnInit() {
+    this.onGetAllProduits();
   }
+
+  onGetAllProduits()  {
+    this.catalogueService.getAllProduits()
+    .subscribe(data => {
+        this.produits = data;
+    }, err => {
+      console.log('problem  to load produits ', err);
+    });
+  }
+
+  onDeleteProd(p) {
+    const conf = confirm('Etes vous sure ?');
+    if (!conf) { return; }
+    this.catalogueService.deleteRessource(p._links.self.href)
+        .subscribe(data => {
+          this.onGetAllProduits();
+        }, err => {
+          console.log('Produit not deleted !!', err);
+        });
+
+  }
+
+  onAddProd() {
+    this.mode = 'new-prod';
+
+  }
+ 
+  onSaveProd(data) {
+    console.log('new Produit!!', data);
+    const url = this.catalogueService.host + '/produits';
+    this.catalogueService.postRessource(url, data)
+         .subscribe(data => {
+            this.onGetAllProduits();
+            this.mode = 'list';
+         }, err => {
+           console.log('add categorie has failed !!', err);
+         });
+  }
+
+
+  onUpdateProd(data) {
+ 
+    this.catalogueService.putRessource(this.currentProduit._links.self.href, data)
+         .subscribe(data => {
+            this.onGetAllProduits();
+            this.mode = 'list';
+         }, err => {
+           console.log('add product has failed !!', err);
+         });
+  }
+
+  
+  onEditProd(prd) {
+     this.catalogueService.getRessource(prd._links.self.href)
+        .subscribe(data => {
+        this.currentProduit = data;
+        this.mode = 'edit-prod';
+        }, err => {
+          console.log('problem was happen wen edit produit', err);
+        });
+
+  }
+
 
 }
--- src/app/catalogue.service.ts
+++ src/app/catalogue.service.ts
@@ -6,20 +6,23 @@ import { AuthenticationService } from './authentication.service';
   providedIn: 'root'
 })
 export class CatalogueService {
+  
 
   public host: String = 'http://localhost:8080';
 
   constructor(private http: HttpClient,
               private authService: AuthenticationService) { }
 
-
+  getAllProduits(): any {
+    return this.http.get(this.host + '/produits');
+   }
+   
   getAllCategories() {
     return this.http.get(this.host + '/categories');
    }
 
    getRessource(url) {
 
-    console.log('77777777777777', url);
     return this.http.get(url);
    }
 
